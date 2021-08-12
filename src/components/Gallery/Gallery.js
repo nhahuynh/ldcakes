f3ed7acd09e36a1f8ca4351661ced13a36aebdfb
nhuynh <nhahuynhwrites@gmail.com>
2021-08-12 16:05:59 -0500

    changed img src to include ldcakes in path

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -61,7 +61,11 @@ function Gallery() {
       <div className="container">
         {filteredImages.map((image) => (
           <div key={image.id} className="image-card">
-            <img className="image" src={`/images/${image.imageName}`} alt="" />
+            <img
+              className="image"
+              src={`ldcakes/images/${image.imageName}`}
+              alt=""
+            />
           </div>
         ))}
       </div>
