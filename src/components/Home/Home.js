9b094e85be0f38bb06036d59c70a277f140ed6bd
nhuynh <nhahuynhwrites@gmail.com>
2021-08-22 17:23:13 -0500

    Revert "getting the logo to work"
    This reverts commit 573765d7444e5ae22074ca738c5e76e32814ce88.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,10 +16,8 @@ class Home extends React.Component {
     return (
       <div>
         <section className="hero">
-          <div className="hero-introduction flex">
-            <div className="hero-logo">
-              <img src={logo} alt="brand logo" />
-            </div>
+          <div className="hero-introduction flex">          
+            <img src={logo} alt="brand logo" />
             <p>
               A unique dessert to wow your guests at parties. <br /> A
               scrumptious treat for yourself and your family.
